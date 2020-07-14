bffc51fda0650bf0af0fdee38468d66150a65bf2
Nick Khurana <khurananick@gmail.com>
2020-07-14 14:30:17 -0400

    minor fix

--- glob.py
+++ glob.py
@@ -4,5 +4,5 @@ ELASTICSEARCH_TREE_LIVE_INDEX_NAME  = "studio_execution_logs_tree_live"
 PERIOD_IN_MINUTES                   = 5 # (60*24*15) # 15 days
 REFRESH_RATE_IN_SECONDS             = 15
 FAKER_REFRESH_RATE_IN_SECONDS       = 2
-ELASTICSEARCH_AUTOPILOT_INDEX_NAME      = "studio_execution_logs"
-ELASTICSEARCH_AUTOPILOT_TREE_INDEX_NAME = "studio_execution_logs_tree"
+ELASTICSEARCH_AUTOPILOT_INDEX_NAME      = "autopilot_execution_logs"
+ELASTICSEARCH_AUTOPILOT_TREE_INDEX_NAME = "autopilot__execution_logs_tree"
